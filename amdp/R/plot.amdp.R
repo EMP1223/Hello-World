8df5dea931e0e8f3b438965c12feee8ec91b9a9a
Alex Lauf Goldstein <alex@convexity.(none)>
2013-07-10 15:47:04 -0400

    added man/ directory with man files, put source in amdp/R/

--- amdp/R/plot.amdp.R
+++ amdp/R/plot.amdp.R
@@ -1,4 +1,5 @@
-plot.amdp = function(amdpobj, xtest_margin = 0, plot_margin = 0.05, pct_to_plot = 1, plot_orig_pts = TRUE, colorvec, ...){
+plot.amdp = function(amdpobj, xtest_margin = 0, plot_margin = 0.05, pct_to_plot = 1, plot_orig_pts = TRUE,
+					colorvec, x_quantile = FALSE, ...){
 
 	#some argument checking
 	if(class(amdpobj)!="amdp")					{ stop("object is not of class 'amdp'")}
@@ -6,6 +7,11 @@ plot.amdp = function(amdpobj, xtest_margin = 0, plot_margin = 0.05, pct_to_plot 
 
 	#extract the grid and lines to plot	
 	grid = amdpobj$gridpts; n_grid = length(grid)
+	ecdf_fcn = NULL
+	if(x_quantile){
+		ecdf_fcn = ecdf(grid)
+		grid = ecdf_fcn(grid)
+	}
 	apdps = amdpobj$apdps; N = nrow(apdps)
 
 	if(missing(colorvec)){
@@ -32,17 +38,26 @@ plot.amdp = function(amdpobj, xtest_margin = 0, plot_margin = 0.05, pct_to_plot 
 	} else {
 		xlab = amdpobj$predictor					#the actual name of the feature.
 	}
+	if(x_quantile){
+		xlab = paste("quantile(", xlab, ")", sep = "")
+	}
 
 	#plot all the prediction lines
 	plot(grid, apdps[1, ], type = "n", ylim = c(min_apdps, max_apdps), xlab = xlab, ylab = expression(hat(y)))
 
-	for( i in 1:N ){
+	for( i in 1 : nrow(apdps) ){
 		points(grid, apdps[i, ], col = colorvec[i], type = "l")
 	}
 
 	if (plot_orig_pts){ #indicate the fitted values associated with observed xj values
 		yhat_actual = amdpobj$actual_prediction[plot_points_indices]
-		xj = amdpobj$xj[plot_points_indices]
-		points(xj, yhat_actual, col = "black", pch = 16, cex = 1.5)
+		
+		if(x_quantile){
+			xj = ecdf_fcn(amdpobj$xj)[plot_points_indices]
+		}else{
+			xj = amdpobj$xj[plot_points_indices]
+		}
+		points(xj, yhat_actual, col = "black", pch = 16, cex = 1.5) #this cex should be a function of the number of lines.
 	}
 }
+
