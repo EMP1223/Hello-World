a2932137590563f8208aec0cc735bc1bc469725c
Alex Lauf Goldstein <alex@convexity.(none)>
2013-07-19 19:09:13 -0400

    Changed argument name to "frac_to_plot" for consistency.

--- amdp/R/plot.amdp.R
+++ amdp/R/plot.amdp.R
@@ -1,9 +1,9 @@
-plot.amdp = function(amdpobj, xtest_margin = 0, plot_margin = 0.05, pct_to_plot = 1, plot_orig_pts = TRUE,
-					colorvec, x_quantile = FALSE, ...){
+plot.amdp = function(amdpobj, xtest_margin = 0, plot_margin = 0.05, frac_to_plot = 1, plot_orig_pts = TRUE,
+					colorvec, x_quantile = FALSE, plot_pdp = FALSE, ...){
 
 	#some argument checking
 	if(class(amdpobj)!="amdp")					{ stop("object is not of class 'amdp'")}
-	if(pct_to_plot <= 0 || pct_to_plot > 1 )		{ stop("pct_to_plot must be in (0,1]")}
+	if(frac_to_plot <= 0 || frac_to_plot > 1 )		{ stop("frac_to_plot must be in (0,1]")}
 
 	#extract the grid and lines to plot	
 	grid = amdpobj$gridpts; n_grid = length(grid)
@@ -23,8 +23,11 @@ plot.amdp = function(amdpobj, xtest_margin = 0, plot_margin = 0.05, pct_to_plot 
 	}
 	
 	#pull out a fraction of the lines to plot
-	plot_points_indices = which(as.logical(rbinom(N, 1, pct_to_plot)))
+	plot_points_indices = which(as.logical(rbinom(N, 1, frac_to_plot)))
 	apdps = apdps[plot_points_indices, ]
+	if(nrow(apdps) == 0){
+		stop("frac_to_plot too small.")
+	}
 	colorvec = colorvec[plot_points_indices]
 	
 	##### now start plotting
@@ -47,6 +50,13 @@ plot.amdp = function(amdpobj, xtest_margin = 0, plot_margin = 0.05, pct_to_plot 
 
 	for( i in 1 : nrow(apdps) ){
 		points(grid, apdps[i, ], col = colorvec[i], type = "l")
+	}
+
+	#if plot_pdp is true, plot actual pdp (in the sense of Friedman '01)
+	if(plot_pdp){
+		#compute them!
+		friedman_pdp = apply(apdps, 2, mean) # pdp = average over the columns
+		points(grid, friedman_pdp, col = "BLACK", type = "l", lwd = 2.5)
 	}
 
 	if (plot_orig_pts){ #indicate the fitted values associated with observed xj values
