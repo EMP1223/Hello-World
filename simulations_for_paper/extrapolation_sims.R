c40e1dd78e7a3fcbe61ab299b27e66cfeae24616
Alex Lauf Goldstein <alex@convexity.(none)>
2013-07-31 21:56:19 -0400

    progress on an extrapolation example.

--- simulations_for_paper/extrapolation_sims.R
+++ simulations_for_paper/extrapolation_sims.R
@@ -15,11 +15,11 @@ ex2a_sim = function(n,seednum=NULL){
 	p = 3
 	X = as.data.frame(matrix(runif(n*p,min=-1,max=1), ncol = p, nrow = n))
 	X[, 2] = (X[,1] < 0)*abs(X[,2])
-	X[, 3] = (X[,2]!=0)*1
+	X[, 3] = (X[,1] > 0)*abs(X[,3])
 		
 	X = X[order(X[, p]), ]
 	colnames(X) = paste("x_", 1 : p, sep = "")
-	bbeta1 = as.matrix(c(5, 2, 0))
+	bbeta1 = as.matrix(c(4, 2, 2))
 
 	#make X dependent...
 	y = as.matrix(X) %*% bbeta1	
@@ -36,13 +36,11 @@ y  = ex2_data$y
 
 #build all models
 rf_mod = randomForest(X, y)
-gam_mod = gam(y~., data=Xy, trace=F)
-lm_mod = lm(y~., data=Xy)
+gbm_mod = gbm(y ~ ., data = Xy, n.tree = 500, interaction.depth = 3, shrinkage = 0.1, cv.folds = 5)
 
 #amdps
 a1_rf = amdp(rf_mod, X, predictor = 1, frac_to_build = 0.3)
-a1_lm = amdp(lm_mod, X, predictor = 1, frac_to_build = 0.3)
-a1_gam = amdp(gam_mod, X, y = Xy$y, predictor = 1, frac_to_build = 0.3)
+a1_gbm = amdp(gbm_mod, X, predictor = 1, frac_to_build = 0.3)
 
 par(mfrow=c(1,2))
 
@@ -61,7 +59,7 @@ for(i in 1:100){
 	cat(i); cat(" ")
 }
 
-gbm_mod = gbm(y ~ ., data = Xy, n.tree = 500, interaction.depth = 3, shrinkage = 0.1, cv.folds = 5)
+
 ntree = gbm.perf(gbm_mod, method = "cv")
 summary(gbm_mod)
 lm_mod = lm(y ~ . * . * ., Xy)
